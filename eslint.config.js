9137e8f3aae63cde7c0f2c18b273e2bdfdaa499d
frndhoon <frndhoon@gmail.com>
2024-12-04 15:11:35 +0900

    chore: eslint & prettier 설정

--- eslint.config.js
+++ eslint.config.js
@@ -1,28 +1,35 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
+import eslint from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import eslintConfigPrettier from 'eslint-config-prettier';
+import reactRecommended from 'eslint-plugin-react/configs/recommended.js';
+import reactHooks from 'eslint-plugin-react-hooks';
+import airbnb from 'eslint-config-airbnb';
+import airbnbTypeScript from 'eslint-config-airbnb-typescript';
 
 export default tseslint.config(
-  { ignores: ['dist'] },
+  eslint.configs.recommended,
+  ...tseslint.configs.recommended,
+  reactRecommended,
+  airbnb,
+  airbnbTypeScript,
   {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
-    files: ['**/*.{ts,tsx}'],
-    languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
-    },
     plugins: {
       'react-hooks': reactHooks,
-      'react-refresh': reactRefresh,
     },
     rules: {
-      ...reactHooks.configs.recommended.rules,
-      'react-refresh/only-export-components': [
-        'warn',
-        { allowConstantExport: true },
-      ],
+      'react-hooks/rules-of-hooks': 'error',
+      'react-hooks/exhaustive-deps': 'warn',
+      'react/react-in-jsx-scope': 'off',
+      'react/prop-types': 'off',
+      'no-console': 'warn',
+      'no-unused-vars': 'warn',
+      'max-len': ['warn', { code: 100 }],
+      'import/prefer-default-export': 'off',
+      'import/extensions': 'off',
     },
   },
-)
+  eslintConfigPrettier,
+  {
+    ignores: ['dist/*'],
+  },
+);
