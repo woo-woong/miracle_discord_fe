e3fac57e37473c79ace7cebd2f1bee2a63db2712
frndhoon <frndhoon@gmail.com>
2024-12-09 15:30:45 +0900

    feat: kakao oauth redirect page 추가 token은 localStoarge에 저장

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,9 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { RootLayout } from '@/layouts/RootLayout';
 import { HomePage } from '@/pages/HomePage';
 import { LoginPage } from '@/pages/LoginPage';
+import { KakaoOAuthRedirectPage } from '@/pages/KakaoOAuthRedirectPage';
+
+const KAKAO_REDIRECT_URL = import.meta.env.VITE_KAKAO_REDIRECT_URL;
 function App() {
   return (
     <BrowserRouter>
@@ -9,6 +12,7 @@ function App() {
         <Routes>
           <Route path='/' element={<HomePage />} />
           <Route path='/login' element={<LoginPage />} />
+          <Route path={new URL(KAKAO_REDIRECT_URL).pathname} element={<KakaoOAuthRedirectPage />} />
         </Routes>
       </RootLayout>
     </BrowserRouter>
