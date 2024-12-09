e9058ac6006fb26d0901bbc8e9dd281bcd82212c
frndhoon <frndhoon@gmail.com>
2024-12-09 13:24:13 +0900

    style: 카카오톡으로 시작하기 btn style 변경

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -2,8 +2,8 @@ import kakaoLogo from '@/assets/Kakaologo.png';
 
 export const LoginPage = () => {
   return (
-    <div className='w-[300px]'>
-      <button className='flex flex-row items-center justify-center gap-2 px-5 py-2 border-[1px] border-gray-200 rounded-lg'>
+    <div className='flex flex-col items-center justify-center h-[300px]'>
+      <button className='flex flex-row items-center justify-center gap-2 px-5 py-2 border-[1px] border-gray-200 rounded-lg hover:bg-gray-100'>
         <img src={kakaoLogo} alt='카카오톡 로고' className='w-10 h-10' />
         <div>카카오톡으로 시작하기</div>
       </button>
